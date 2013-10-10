992ebf9a48a7697820e5acaa670c11fd974f7234
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-09 23:00:25 -0400

    checking package file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,14 @@ module.exports = function (grunt) {
             'default': {
                 src: [ 'index.js' ]
             }
+        },
+        'nice-package': {
+            all: {}
         }
     });
 
     var plugins = module.require('matchdep').filterDev('grunt-*');
     plugins.forEach(grunt.loadNpmTasks);
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['nice-package', 'jshint']);
 };
