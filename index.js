b974c97030f429fbb1d5ded27b51f9350b3118be
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-09 22:52:21 -0400

    fixed jshint

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var exec = require('child_process').exec;
 function execPromise(cmd) {
 	var deferred = Q.defer();
 	exec(cmd, function (err, stdout, stderr) {
-		if (err) return deferred.reject(stderr);
+		if (err) {
+			return deferred.reject(stderr);
+		}
 		deferred.resolve(stdout);
 	});
 	return deferred.promise;
