34196e248373d71688b8f8f9ac92b6649a900170
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-09 22:08:57 -0400

    preparing for testing promises

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,8 +13,8 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-bump');
+    var plugins = module.require('matchdep').filterDev('grunt-*');
+    plugins.forEach(grunt.loadNpmTasks);
 
     grunt.registerTask('default', ['jshint']);
 };
