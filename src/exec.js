1c45da37c2135a4b101b2c38a8fd9612ebd9b807
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-13 22:52:37 -0500

    installed latest dependencies

--- src/exec.js
+++ src/exec.js
@@ -1,7 +1,9 @@
 var Q = require('q');
 var exec = require('child_process').exec;
+var verify = require('check-types').verify;
 
 function execPromise(cmd) {
+  verify.unemptyString(cmd, 'missing command to execute');
   var deferred = Q.defer();
   exec(cmd, function (err, stdout, stderr) {
     if (err) {
