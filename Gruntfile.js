46350c2c9980551d338ce1ad0d8eff7bea9713ec
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-13 19:04:06 -0500

    better gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,60 @@
 /*global module:false*/
 module.exports = function (grunt) {
+  require('time-grunt')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+      },
+      'default': {
+        src: [ 'index.js' ]
+      }
+    },
+    'nice-package': {
+      all: {}
+    },
+    'node-qunit': {
+      all: {
+        deps: './node_modules/qunit-promises/qunit-promises.js',
+        code: './src/getOneLineLog.js',
+        tests: './src/test/getOneLineLog.js'
+      }
+    },
+    complexity: {
+      default: grunt.file.readJSON('complexity.json')
+    },
+    readme: {
+      options: {
+        readme: './docs/README.tmpl.md',
+        docs: '.',
+        templates: './docs'
+      }
+    },
+    /* to bump version, then run grunt (to update readme), then commit
+    grunt release
+    */
+    bump: {
+      options: {
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'], // '-a' for all files
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    }
+  });
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-            },
-            'default': {
-                src: [ 'index.js' ]
-            }
-        },
-        'nice-package': {
-            all: {}
-        },
+  var plugins = module.require('matchdep').filterDev('grunt-*');
+  plugins.forEach(grunt.loadNpmTasks);
 
-        'node-qunit': {
-            all: {
-                deps: './node_modules/qunit-promises/qunit-promises.js',
-                code: './src/getOneLineLog.js',
-                tests: './src/test/getOneLineLog.js'
-            }
-        }
-    });
+  grunt.registerTask('pre-check', ['deps-ok', 'jsonlint',
+    'jshint', 'nice-package', 'complexity']);
+  grunt.registerTask('release', ['bump-only:patch', 'readme', 'bump-commit']);
+  grunt.registerTask('test', 'node-qunit');
 
-    var plugins = module.require('matchdep').filterDev('grunt-*');
-    plugins.forEach(grunt.loadNpmTasks);
-
-    grunt.registerTask('default', ['nice-package', 'jshint', 'node-qunit']);
+  grunt.registerTask('default', 'pre-check', 'test', 'readme');
 };
