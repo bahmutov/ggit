8b2737dd39c6cbd86d8283dfe6d2ff8681599433
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-09 23:16:14 -0500

    testing repo root

--- src/repo-root.js
+++ src/repo-root.js
@@ -1,29 +1,11 @@
-var spawn = require('child_process').spawn;
-var check = require('check-types');
+var exec = require('./exec');
 
 module.exports.getGitRootFolder = function (cb) {
-	check.verify.fn(cb, 'expect callback function, not', cb);
-	var git = spawn('git', ['rev-parse', '--show-toplevel']);
-	var topLevelFolder = null;
-	var err;
-
-	git.stdout.setEncoding('utf-8');
-	git.stdout.on('data', function (data) {
-		data.trim();
-		if (/fatal/.test(data)) {
-			err = 'Could not determine git top folder\n' + data;
-			return;
-		}
-		topLevelFolder = data.trim();
-	});
-
-	git.stderr.setEncoding('utf-8');
-	git.stderr.on('data', function (data) {
-		err = 'Could not determine git top folder\n' + data;
-		return;
-	});
-
-	git.on('exit', function () {
-		cb(err, topLevelFolder);
-	});
+	return exec('git rev-parse --show-toplevel')
+		.then(function (path) {
+			cb(null, path.trim());
+			return path;
+		}, function (err) {
+			cb(err);
+		});
 };
--- src/test/repoRoot.js
+++ src/test/repoRoot.js
@@ -1,3 +1,6 @@
+require('lazy-ass');
+var check = require('check-types');
+
 var root = require('../repo-root').getGitRootFolder;
 
 gt.module('repo root');
@@ -8,17 +11,17 @@ gt.test(function repoRootFunction() {
 
 gt.async(function thisRoot() {
 	root(function (err, pathname) {
-		gt.undefined(err, 'there is not error');
-		gt.string(pathname, 'expected to get repo path');
-		gt.start();
-	});
+		console.log('this root', pathname);
+		la(!err, 'there is not an error', err);
+		la(check.string(pathname), 'expected to get repo path', pathname);
+	}).finally(gt.start);
 });
 
 gt.async(function outOfGitRoot() {
-	process.chdir('../../..');
+	process.chdir('../../../..');
 	root(function (err, pathname) {
-		gt.ok(err, 'there was an error!');
-		gt.ok(!pathname, 'there is no repo root path');
-		gt.start();
-	});
+		console.log('this root', pathname);
+		la(err, 'there was an error!');
+		la(!pathname, 'there is no repo root path', pathname);
+	}).finally(gt.start);
 });
