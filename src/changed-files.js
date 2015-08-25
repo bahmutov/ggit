92bb2d7c26bb86b7a5c994dc3246f2d38018f2eb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-25 08:32:34 -0400

    getting modification type with the filename

--- src/changed-files.js
+++ src/changed-files.js
@@ -1,17 +1,32 @@
 var exec = require('./exec');
 var log = require('debug')('ggit');
+var _ = require('lodash');
 
 exports.changedFiles = changedFiles;
 
+function parseLine(line) {
+	var parts = line.split('\t');
+	return {
+		diff: parts[0],
+		name: parts[1]
+	};
+}
+
 function changedFiles() {
-	var cmd = 'git diff --name-only';
+	var gitCommand = _.template('git diff --name-status --diff-filter=<%= filter %>');
+	log('filter letters Added (A), Copied (C), Deleted (D), Modified (M)');
+	var filter = 'AMCD';
+	var cmd = gitCommand({ filter: filter });
+	log('changed files command', cmd);
+
 	return exec(cmd)
 		.then(function (data) {
 			data.trim();
 			var files = data.split('\n');
 			files = files.filter(function (filename) {
 				return filename.length;
-			});
+			}).map(parseLine);
+
 			log('found changed files');
 			log(files);
 			return files;
