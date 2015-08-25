79d49cc156aa736566e2959720f3463aeca026db
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-25 08:24:31 -0400

    changed files returns a promise

--- src/changed-files.js
+++ src/changed-files.js
@@ -1,38 +1,25 @@
-var spawn = require('child_process').spawn;
-var check = require('check-types');
+var exec = require('./exec');
 var log = require('debug')('ggit');
 
 exports.changedFiles = changedFiles;
 
-function changedFiles(cb) {
-	check.verify.fn(cb, 'expect callback function, not', cb);
-
-	var diff = spawn('git', ['diff', '--name-only']);
-	var files = [];
-
-	diff.stdout.setEncoding('utf-8');
-	diff.stdout.on('data', function (data) {
-		data.trim();
-		files = data.split('\n');
-		files = files.filter(function (filename) {
-			return filename.length;
+function changedFiles() {
+	var cmd = 'git diff --name-only';
+	return exec(cmd)
+		.then(function (data) {
+			data.trim();
+			var files = data.split('\n');
+			files = files.filter(function (filename) {
+				return filename.length;
+			});
+			log('found changed files');
+			log(files);
+			return files;
 		});
-		log('found changed files');
-		log(files);
-	});
-
-	diff.stderr.setEncoding('utf-8');
-	diff.stderr.on('data', function (data) {
-		console.error('git diff error: ' + data);
-	});
-
-	diff.on('exit', function () {
-		cb(files);
-	});
 }
 
 if (!module.parent) {
-	changedFiles(function (files) {
+	changedFiles().then(function (files) {
 		console.log('changed files in the current repo');
 		console.log(files);
 	});
