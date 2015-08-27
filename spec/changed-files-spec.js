1fb9a5ddedcdeab6973a5de1e82099a33d8010de
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-26 21:58:34 -0400

    better testing, no need for global grunt

--- spec/changed-files-spec.js
+++ spec/changed-files-spec.js
@@ -1,9 +1,9 @@
 require('lazy-ass');
 var check = require('check-more-types');
-var testFn = require('describe-function');
+var describeIt = require('describe-it');
 var filename = __dirname + '/../src/changed-files';
 
-testFn(filename, 'parseLine(line)', function (getParseLine) {
+describeIt(filename, 'parseLine(line)', function (getParseLine) {
   it('parses line', function () {
     var parse = getParseLine();
     la(check.fn(parse));
@@ -14,9 +14,18 @@ testFn(filename, 'parseLine(line)', function (getParseLine) {
   });
 });
 
-testFn(filename, 'var stdoutToGrouped', function (getTransform) {
+describeIt(filename, 'var stdoutToGrouped', function (getTransform) {
   it('correctly parses output', function () {
     var gitOutput = 'M\tfoo.js';
+    var result = getTransform()(gitOutput);
+    la(check.object(result) && check.array(result.M),
+      'has modified file list', result);
+    la(result.M.length === 1, 'single modified file');
+    la(result.M[0].name === 'foo.js');
+  });
+
+  it('ignores new lines', function () {
+    var gitOutput = '\n\nM\tfoo.js\n\n';
     var result = getTransform()(gitOutput);
     la(check.object(result) && check.array(result.M),
       'has modified file list', result);
