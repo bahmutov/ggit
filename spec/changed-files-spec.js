c9846bd652f9003c8521b20e0c95d86b0577fba6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-11 09:04:03 -0500

    chore(test): added simple unit test

--- spec/changed-files-spec.js
+++ spec/changed-files-spec.js
@@ -1,2 +1,9 @@
-require('lazy-ass');
-var check = require('check-more-types');
+var la = require('lazy-ass');
+var is = require('check-more-types');
+
+describe('changd files', function () {
+  var changedFiles = require('..').changedFiles;
+  it('is a function', function () {
+    la(is.fn(changedFiles));
+  });
+});
