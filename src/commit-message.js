194757b60b53dd069decae9d79f078caad015832
Ryan Kimber <ryan.kimber@formhero.io>
2016-03-20 15:35:25 -0400

    fix(GitLocation): Added handling for .git file if repository is submodule
    The existing implementation didn't handle the case where .git is actually a file telling us where
    the real .git files are.

--- src/commit-message.js
+++ src/commit-message.js
@@ -1,21 +1,15 @@
 var Q = require('q');
 var exists = require('fs').existsSync;
-var fileInfo = require('fs').lstatSync;
 var read = require('fs').readFileSync;
-var filename = './.git/COMMIT_EDITMSG';
+var filename = require('./git-folder')() + '/COMMIT_EDITMSG';
 
 function commitMessage() {
-  var commitMsgFile = filename;
-  if(!fileInfo('./.git').isDirectory()) {
-    var unparsedText = '' + read('./.git');
-    commitMsgFile = unparsedText.substring('gitdir: '.length).trim() + '/COMMIT_EDITMSG';
+
+  if (!exists(filename)) {
+      return Q.reject(new Error('Cannot find file ' + filename));
   }
 
-  if (!exists(commitMsgFile)) {
-      return Q.reject(new Error('Cannot find file ' + commitMsgFile));
-  }
-
-  var text = read(commitMsgFile, 'utf8');
+  var text = read(filename, 'utf8');
   /* jshint -W064 */
   return Q(text);
 
