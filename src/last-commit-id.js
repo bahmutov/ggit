c8e9a3d67ef86862630d09bad3d61cf7ed2caced
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-19 22:19:58 -0400

    feat(sha): getting sha from heroku or circle ci if available

--- src/last-commit-id.js
+++ src/last-commit-id.js
@@ -1,3 +1,6 @@
+'use strict';
+
+var Promise = require('bluebird');
 var la = require('lazy-ass');
 var check = require('check-more-types');
 var exec = require('./exec');
@@ -5,11 +8,45 @@ var exec = require('./exec');
 function saveIntoFile(options, id) {
   if (check.unemptyString(options.file)) {
     var write = require('fs').writeFileSync;
-    var contents = JSON.stringify({ id: id }, null, 2);
+    var contents = JSON.stringify({ id: id }, null, 2) + '\n';
     write(options.file, contents, 'utf8');
     console.log('saved last commit %s in file %s', id, options.file);
   }
   console.log('last commit:', id);
+}
+
+var env = process.env;
+
+function getHerokuCommit() {
+  if (env.SOURCE_VERSION &&
+    check.unemptyString(env.SOURCE_VERSION)) {
+    return env.SOURCE_VERSION;
+  }
+}
+
+function getCircleCiCommit() {
+  if (env.CIRCLE_SHA1 &&
+    check.unemptyString(env.CIRCLE_SHA1)) {
+    return env.CIRCLE_SHA1;
+  }
+}
+
+function getGitCommit() {
+  var cmd = 'git log --format="%H" -n 1';
+  return exec(cmd);
+}
+
+function findCommit() {
+  var ciSha = getHerokuCommit() || getCircleCiCommit();
+  if (ciSha) {
+    return Promise.resolve(ciSha);
+  }
+  return getGitCommit();
+}
+
+function cleanOutput(str) {
+  la(check.unemptyString(str), 'expected commit id string', str);
+  return str.trim();
 }
 
 /*
@@ -17,16 +54,13 @@ function saveIntoFile(options, id) {
 */
 function lastCommitId(options) {
   options = options || {};
-  var cmd = 'git log --format="%H" -n 1';
-  return exec(cmd)
-    .then(function cleanOutput(str) {
-      la(check.unemptyString(str), 'expected commit id string', str);
-      return str.trim();
-    })
-    .then(function (id) {
-      saveIntoFile(options, id);
-      return id;
-    });
+  function saveWithOptions(id) {
+    saveIntoFile(options, id);
+    return id;
+  }
+  return findCommit()
+    .then(cleanOutput)
+    .then(saveWithOptions);
 }
 
 module.exports = lastCommitId;
