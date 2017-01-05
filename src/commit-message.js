f76c158a1289aca3d0ee3473ed2b87f080334ba2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-04 23:25:24 -0500

    fix(effects): defer commit-message .git folder, close #41

--- src/commit-message.js
+++ src/commit-message.js
@@ -1,10 +1,9 @@
 var Q = require('q');
 var exists = require('fs').existsSync;
 var read = require('fs').readFileSync;
-var filename = require('./git-folder')() + '/COMMIT_EDITMSG';
 
 function commitMessage() {
-
+  var filename = require('./git-folder')() + '/COMMIT_EDITMSG';
   if (!exists(filename)) {
       return Q.reject(new Error('Cannot find file ' + filename));
   }
