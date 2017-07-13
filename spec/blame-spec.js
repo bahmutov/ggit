b2bf285c40b0c17743d84495b69c51d4c6367089
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-13 07:30:29 -0400

    mock test

--- spec/blame-spec.js
+++ spec/blame-spec.js
@@ -1,6 +1,11 @@
-const la = require('lazy-ass');
-const is = require('check-more-types');
+const la = require('lazy-ass')
+const is = require('check-more-types')
+const schemaShot = require('schema-shot')
+const snapshot = require('snap-shot')
+const { stubExecOnce } = require('stub-spawn-once')
+const { stripIndent } = require('common-tags')
 
+/* global describe, it */
 describe.only('blame', () => {
   const blame = require('..').blame
 
@@ -10,11 +15,31 @@ describe.only('blame', () => {
 
   it('gets blame for 1 line of this file', () => {
     const line = 1
-    return blame(__filename, line)
-      .then(info => {
-        la(is.object(info))
-        console.log(info)
-      })
+    return schemaShot(blame(__filename, line))
+  })
+
+  it('mocks exec and parses output', () => {
+    // line 10000 does NOT exist in this file!
+    const line = 10000
+    const cmd = `git blame --porcelain -L ${line},${line} ${__filename}`
+    const output = stripIndent`
+      adfb30d5888bb1eb9bad1f482248edec2947dab6 ${line} ${line} 1
+      author Gleb Bahmutov
+      author-mail <gleb.bahmutov@gmail.com>
+      author-time 1499944626
+      author-tz -0400
+      committer Gleb Bahmutov
+      committer-mail <gleb.bahmutov@gmail.com>
+      committer-time 1499944626
+      committer-tz -0400
+      summary mock commit
+      filename spec/blame-spec.js
+      this is mock line ${line}
+    `
+    stubExecOnce(cmd, output)
+    // can use exact snapshot here, because the output will always be
+    // the same! See __snapshots__/blame-spec.js.snap-shot
+    return snapshot(blame(__filename, line))
   })
 })
 
